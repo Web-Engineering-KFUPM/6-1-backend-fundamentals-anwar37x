d3f9d9bd84df655cdd80baf78005f60918a807f0
anwar37x <s202242640@kfupm.edu.sa>
2026-03-30 20:34:47 +0300

    2

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,25 +1,25 @@
-// TODO 1: Import Express
+import express from "express";
+import cors from "cors";
 
+const app = express();
 
+app.use(cors());
 
-// TODO 2: Create the Express app and store it in a variable named app
+app.get("/", (req, res) => {
+    res.send("Hello from the back-end server");
+});
 
+app.get("/about", (req, res) => {
+    res.send("This is the about route");
+});
 
+app.get("/student", (req, res) => {
+    res.json({
+        name: "Anwar Almutairi",
+        major: "Software Engineering",
+    });
+});
 
-// TODO 3: Allow React to access the server
-
-
-
-// TODO 5: Create the home route "/"
-
-
-
-// TODO 6: Create the "/about" route
-
-
-
-// TODO 7: Create the "/student" route
-
-
-
-// TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
